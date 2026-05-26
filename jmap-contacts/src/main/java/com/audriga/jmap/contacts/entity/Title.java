84c358697c541f86c3e1ffeb076f688d105181b3
Luna Schwalbe <luna@audriga.com>
2026-05-26 11:50:59 +0200

    contacts: add missing organizationId field to Title

--- jmap-contacts/src/main/java/com/audriga/jmap/contacts/entity/Title.java
+++ jmap-contacts/src/main/java/com/audriga/jmap/contacts/entity/Title.java
@@ -3,7 +3,11 @@ package com.audriga.jmap.contacts.entity;
 import com.audriga.jmap.annotation.Default;
 import com.audriga.jmap.annotation.Type;
 import lombok.Builder;
+import org.jspecify.annotations.Nullable;
 
 @Builder(toBuilder = true)
 @Type
-public record Title(String name, @Default("\"title\"") String kind) {}
+public record Title(
+        String name,
+        @Default("\"title\"") String kind,
+        @Nullable String organizationId) {}
