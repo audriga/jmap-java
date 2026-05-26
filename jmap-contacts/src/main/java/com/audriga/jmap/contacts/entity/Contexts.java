99657276f4f9c75891d13d9f224c8a099d03dd0f
Luna Schwalbe <luna@audriga.com>
2026-05-26 11:50:59 +0200

    contacts: add useful `of` methods to Contexts

--- jmap-contacts/src/main/java/com/audriga/jmap/contacts/entity/Contexts.java
+++ jmap-contacts/src/main/java/com/audriga/jmap/contacts/entity/Contexts.java
@@ -2,6 +2,15 @@ package com.audriga.jmap.contacts.entity;
 
 import com.audriga.jmap.annotation.Inline;
 import java.util.Set;
+import org.jspecify.annotations.Nullable;
 
 @Inline
-public record Contexts(Set<String> values) {}
+public record Contexts(Set<String> values) {
+    public static @Nullable Contexts ofNullable(@Nullable String value) {
+        return value == null ? null : new Contexts(Set.of(value));
+    }
+
+    public static Contexts of(String... values) {
+        return new Contexts(Set.of(values));
+    }
+}
