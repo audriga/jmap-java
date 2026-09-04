6cc6a05847bb254aa8b528d0a0b6a90b0594adc7
Puja Karmacharya <pujakarmacharya17@gmail.com>
2026-09-04 16:11:32 +0200

    Register missing LocalDateTime Gson adapter
    CalendarEvent.start, recurrenceId, recurrenceOverrides keys, and
    RecurrenceRule.until are all LocalDateTime, sent as plain ISO strings
    by servers. Without a registered adapter, Gson fell back to default
    reflection parsing, which expected an object instead of a string,
    so parsing these fields failed.

---
+++ jmap-gson/src/main/java/com/audriga/jmap/gson/adapter/LocalDateTimeTypeAdapter.java
@@ -0,0 +1,52 @@
+/*
+ * Copyright 2020 Daniel Gultsch
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package com.audriga.jmap.gson.adapter;
+
+import com.google.gson.GsonBuilder;
+import com.google.gson.TypeAdapter;
+import com.google.gson.stream.JsonReader;
+import com.google.gson.stream.JsonToken;
+import com.google.gson.stream.JsonWriter;
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class LocalDateTimeTypeAdapter extends TypeAdapter<LocalDateTime> {
+
+    public static void register(final GsonBuilder builder) {
+        builder.registerTypeAdapter(LocalDateTime.class, new LocalDateTimeTypeAdapter());
+    }
+
+    @Override
+    public void write(final JsonWriter jsonWriter, final LocalDateTime localDateTime) throws IOException {
+        if (localDateTime == null) {
+            jsonWriter.nullValue();
+        } else {
+            jsonWriter.value(DateTimeFormatter.ISO_LOCAL_DATE_TIME.format(localDateTime));
+        }
+    }
+
+    @Override
+    public LocalDateTime read(final JsonReader jsonReader) throws IOException {
+        if (jsonReader.peek() == JsonToken.NULL) {
+            jsonReader.nextNull();
+            return null;
+        }
+        final String asString = jsonReader.nextString();
+        return LocalDateTime.parse(asString);
+    }
+}
